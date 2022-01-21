ebad4d7c0530a5213d017a5a5bbc19188ef146b3
g20946 <87630207+mana-tsuchiya@users.noreply.github.com>
2022-01-21 22:06:37 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -9,10 +9,9 @@ function clock(){
   mm = mm < 10 ? "0" + mm : mm;
   ss = ss < 10 ? "0" + ss : ss;
 
-  // 日付・時刻の文字列を作成
-  let time = document.getElementById("time");
-  time.textContent = hh + ":" + mm + ":" + ss;
-};
+  let time = `${hh}:${mm}:${ss}`;
+  document.querySelector(".time").innerText = time;
+}
 
 function start(){
   window.setInterval(clock, 1000);

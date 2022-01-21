6df09da5eb84d5bac5f484b3789e81301f6da940
g20946 <87630207+mana-tsuchiya@users.noreply.github.com>
2022-01-21 22:00:34 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,39 +1,19 @@
-const clock = () => {
-  // 現在の日時・時刻の情報を取得
-  const d = new Date();
+function clock(){
+  let d = new Date();
 
-  // 年を取得
-  let year = d.getFullYear();
-  // 月を取得
-  let month = d.getMonth() + 1;
-  // 日を取得
-  let date = d.getDate();
-  // 曜日を取得
-  let dayNum = d.getDay();
-  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  let day = weekday[dayNum];
-  // 時を取得
-  let hour = d.getHours();
-  // 分を取得
-  let min = d.getMinutes();
-  // 秒を取得
-  let sec = d.getSeconds();
+  let hh = d.getHours();
+  let mm = d.getMinutes();
+  let ss = d.getSeconds();
 
-  // 1桁の場合は0を足して2桁に
-  month = month < 10 ? "0" + month : month;
-  date = date < 10 ? "0" + date : date;
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
 
   // 日付・時刻の文字列を作成
-  let today = `${year}.${month}.${date} ${day}`;
-  let time = `${hour}:${min}:${sec}`;
-
-  // 文字列を出力
-  document.querySelector(".clock-date").innerText = today;
-  document.querySelector(".clock-time").innerText = time;
+  let time = document.getElementById("time");
+  time.textContent = hh + ":" + mm + ":" + ss;
 };
 
-// 1秒ごとにclock関数を呼び出す
-setInterval(clock, 1000);
+function start(){
+  window.setInterval(clock, 1000);
+}
